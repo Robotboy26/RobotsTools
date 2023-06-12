20746c447b166affb1cf93784b6e030765449c89
robotboy26 <robotboygamer26@gmail.com>
2023-06-12 07:34:04 -0700

    Changes to be committed:

--- src/RobotsTools/main.py
+++ src/RobotsTools/main.py
@@ -12,29 +12,6 @@ except:
 
 lock = threading.Lock()
 
-
-
-
-
-
-
-
-def log(message=str, LogMessageType=str(getConfigValue("defaultLogMessageType")), filename=str(getConfigValue("defaultLogFile"))):
-    LogMessageType = LogMessageType.upper()
-    if getConfigValue("LogSettings") == True:
-        try:
-            debug(str(message), LogMessageType)
-            with lock:
-                with open(filename, "a") as file:
-                    file.write(formattedWrite(str(message), LogMessageType))
-                    file.write("\n")
-            return "log secceeded"
-        except Exception as e:
-            debug(f"!!! Could not log: {str(e)}, you might need to setLogSettings !!!")
-            return e
-    else:
-        return "LogSettings is False"
-
 from .setSettings import changeDefaultLogFile
 
 def logFile(filename=str, ClearLogFile=bool(getConfigValue("ClearLogFile"))):
